c3f72d92452c02cac49a4e694c4277340906036d
w1032593545 <1032593545@qq.com>
2020-04-23 15:53:48 +0800

    自定义url处理类&数据库创建

--- web/static/js/user/login.js
+++ web/static/js/user/login.js
@@ -5,10 +5,39 @@ var user_login_ops = {
     },
     eventBind:function(){
         $(".do-login").click(function(){
+            var btn_target = $(this)
+            if(btn_target.hasClass("disabled")){
+                alert('请不要重复提交')
+            }
             var login_name = $("input[name=login_name]").val()
             var login_pwd = $("input[name=login_pwd]").val()
-            console.log(login_name);
-            console.log(login_pwd);
+            
+            // 前端校检  不为空，长度不小于1
+            if(login_name == undefined || login_name.length <1){
+                alert('您输入的用户名为空')
+                return;
+            }
+            if(login_pwd == undefined || login_pwd.length <1){
+                alert('您输入的密码为空')
+                return;
+            }
+
+            btn_target.addClass('disabled')
+
+            // Ajax 亲后端分离的，数据格式是json，
+            // 前端发送的数据是json格式的，所以后端response返回的数据也得是json格式的
+            $.ajax({
+                url:"/user/login",
+                type:"POST",
+                data:{"login_name":login_name,"login_pwd":login_pwd},
+                dataType:"json",
+                success:function(response){
+                    btn_target.removeClass("disabled")
+                    console.log(response);
+                    
+                }
+            })
+
         })
         
                 
